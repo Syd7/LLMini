5b343190f0c6066f3e808598ffd0af410f729504
Syd7 <sydpaguio@gmail.com>
2025-08-04 18:46:00 +0800

    simple tokenizer implementation

---
+++ encoder.py
@@ -0,0 +1,31 @@
+import urllib.request
+import re
+
+url = ("https://raw.githubusercontent.com/rasbt/"
+       "LLMs-from-scratch/main/ch02/01_main-chapter-code/"
+       "the-verdict.txt")
+
+file_path = "the-verdict.txt"
+urllib.request.urlretrieve(url, file_path)
+
+
+with open("the-verdict.txt", "r", encoding="utf-8") as f:
+    raw_text = f.read()
+print ("Total number of characters:", len(raw_text))
+print(raw_text[:99])
+
+
+#basic Tokenizer
+preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
+preprocessed = [item.strip() for item in preprocessed if item.strip()]
+
+#sort all unique tokens
+all_words_sorted = sorted(set(preprocessed))
+vocab_size = len(preprocessed)
+print(vocab_size)
+
+vocab = {token:integer for integer, token in enumerate(all_words_sorted)}
+for i, item in enumerate(vocab.items()):
+    print(item)
+    if i >= 50:
+        break
