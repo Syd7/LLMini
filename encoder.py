c59da80ddeee26f4f1a925e56901e2607ca604b6
Syd7 <sydpaguio@gmail.com>
2025-08-06 23:01:30 +0800

    Testing Elementary Attention System with naive Softmax approach for normalization

--- encoder.py
+++ encoder.py
@@ -55,4 +55,42 @@ print(input_embeddings.shape)
 
 #3 Coding Attention Mechanisms
 #1 Simplified Self-Attention #2 Self-Attention #3 Causal Attention #4 Multi-Head Attention
+#Today I will write #1 and #2 and maybe #3 if im feeling quirky
+
+#Self-Attention is a mechanism that allows the model to evaluate each position in the input sequence
+#and the relevancy of other positions in the same sequence when computing the representation
+#one of the hardest aspects of creating an llm is the self-attention
+#Without Training Weights
+
+#simple self_attention without weights
+inputs = torch.tensor(
+    [[0.43, 0.15, 0.89], #Your  x^1
+     [0.55, 0.87, 0.66], #Journey x^2
+     [0.57, 0.85, 0.64], #Starts x^3
+     [0.22, 0.58, 0.33], #With x^4
+     [0.77, 0.25, 0.10], #A x^5
+     [0.05, 0.80, 0.55]] #Step x^6
+)
+
+query = inputs[1]
+attn_scores_2 = torch.empty(inputs.shape[0]) #this will hold the attention scores
+for i, x_i in enumerate(inputs):
+    attn_scores_2[i] = torch.dot(x_i, query) #dot product of query and each input vector
+#Dot Vectors are not only used to convert two vectors into a scalar, but also to measure the similarity between them
+print(attn_scores_2)
+#now lets try and normalize these vectors 
+attn_weights_2_tmp = attn_scores_2/attn_scores_2.sum() #normalize the attention scores just like in physics
+print("Attention Weights", attn_weights_2_tmp)
+print("Sum: ", attn_weights_2_tmp.sum())
+
+#try to use softmax approach
+def softmax_naive(x):
+    return torch.exp(x) / torch.exp(x).sum(dim=0)
+
+attn_weights_2_naive = softmax_naive(attn_scores_2)
+print("Attention Weights (Naive Softmax):", attn_weights_2_naive)
+print("Sum (Naive Softmax):", attn_weights_2_naive.sum()) #alternative way of normalizing attention sore but it may suffer from
+#overflow or underflow
+
+
 
