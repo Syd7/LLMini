b86af636884adfc3fd281079c6471b4cf8ef9670
Syd7 <sydpaguio@gmail.com>
2025-08-14 23:06:35 +0800

    added dummygptmodel for testing

---
+++ Architecture.py
@@ -0,0 +1,11 @@
+GPT_CONFIG_124M = {
+    "vocab_size": 50257,
+    "context_length": 1024,
+    "emb_dim": 768,         #embedding dimension
+    "n_heads": 12,          #Number of Attention Heads
+    "n_layers": 12,         
+    "drop_rate": 0.1,       #DropoutRate
+    "qkv_bias": False       #Query Key Value Bias
+
+
+}
